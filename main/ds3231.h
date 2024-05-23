6307f089e1d0b118fec2d60cbaf6bdb63d87c9d6
TempusFugit05 <adam.agrano@gmail.com>
2024-05-23 16:41:30 +0300

    first commit

---
+++ main/ds3231.h
@@ -0,0 +1,247 @@
+#include "driver/i2c_master.h"
+#include "string.h"
+#include <ctime>
+
+#define DS3231_I2C_ADDRESS          0x68
+#define SECONDS_REGISTER_ADDRESS    0x00
+#define MINUTES_REGISTER_ADDRESS    0x01
+#define HOURS_REGISTER_ADDRESS      0x02
+#define DAYS_REGISTER_ADDRESS       0x03
+#define YEARS_REGISTER_ADDRESS      0x04
+
+
+/*TODO:
+ADD ERROR HANDLING FOR INVALID TIME INPUT/OUTPUT
+ADD FUNCTION TO AUTOMATICALLY CONVERT __TIME__ __DATE__ TO VALID NUMBERS?
+*/
+
+typedef enum{
+    DS3231_TIME_FORMAT_24_HOURS,
+    DS3231_TIME_FORMAT_12_HOURS
+}hour_format_t;
+
+typedef struct
+{
+    i2c_master_dev_handle_t dev_handle;
+    hour_format_t hour_format;
+}ds3231_handle_t;
+
+uint8_t inline bcd_to_decimal(uint8_t bcd_num)
+{
+    return bcd_num - 6 * (bcd_num >> 4); // Convert an int in bcd format to decimal
+}
+
+uint8_t inline decimal_to_bcd(uint8_t decimal_num)
+{
+    return (decimal_num%10)+((decimal_num/10)<<4); // Convert an int in decimal format to bcd
+}
+
+void ds3231_config_hour_format(const ds3231_handle_t ds3231_handle, hour_format_t format)
+{
+    /*Toggle between 12h format and 24h format*/
+
+    i2c_master_dev_handle_t dev_handle = ds3231_handle.dev_handle;
+
+    uint8_t time_buffer[1]; // Buffer to store the recieved time data
+    uint8_t register_num_time_buffer[1] = {HOURS_REGISTER_ADDRESS};
+    i2c_master_transmit_receive(dev_handle, register_num_time_buffer, 1, time_buffer, 1, -1); // Get hour data
+
+    // The sixth bit in the register toggles between the 12h and 24h formats
+    // if bit == 1 -> 12h format
+    if(format == DS3231_TIME_FORMAT_12_HOURS)
+    {
+        *time_buffer = *time_buffer | 0b00100000; // Set sixth bit to 1
+    }
+    else
+    {
+        *time_buffer = ~((~*time_buffer) | 0b00100000); // Set sixth bit to 0
+    }
+
+    uint8_t transmition_buffer[2] = {*register_num_time_buffer, *time_buffer}; // Rewrite the hour data to includ desired format
+    i2c_master_transmit(dev_handle, transmition_buffer, 2, -1);
+}
+
+ds3231_handle_t ds3231_init(i2c_master_bus_handle_t *bus_handle, hour_format_t time_format)
+{
+    /*Initialize the rtc module*/
+
+    // Setup i2c handle
+    i2c_device_config_t conf = 
+    {
+        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+        .device_address = DS3231_I2C_ADDRESS,
+        .scl_speed_hz = 100000
+    };
+    i2c_master_dev_handle_t dev_handle;
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(*bus_handle, &conf, &dev_handle));
+    
+    // Assign relevant information to ds3231 handle
+    ds3231_handle_t ds3231_handle;
+    ds3231_handle.dev_handle = dev_handle;
+    ds3231_handle.hour_format = time_format;
+
+    char* TAG = "ds3231";
+    ESP_LOGI(TAG, "ds3231 initialized");
+
+    return ds3231_handle;
+}
+
+int validate_time(ds3231_handle_t *ds3231_handle, tm *time_struct)
+{
+    /*Function to check that all the time values are within the expected range*/
+    char TAG[] = "ds3231";
+    
+    if (time_struct->tm_sec > 59 or time_struct->tm_sec < 0)
+    {
+        ESP_LOGE(TAG, "Invalid seconds value expected range: 0<=t<=59, got value: %i instead", time_struct->tm_sec);
+        return -1;
+    }
+
+    else if (time_struct->tm_min > 59 or time_struct->tm_min < 0)
+    {
+        ESP_LOGE(TAG, "Invalid minutes value expected range: 0<=t<=59, got value: %i instead", time_struct->tm_min);
+        return -1;
+    }
+
+    else if ((time_struct->tm_hour > 24 or time_struct->tm_hour < 0) && ds3231_handle->hour_format == DS3231_TIME_FORMAT_24_HOURS)
+    {
+        ESP_LOGE(TAG, "Invalid hour value expected range (24h format): 0<=t<=24, got value: %i instead", time_struct->tm_hour);
+        return -1;
+    }
+
+    else if ((time_struct->tm_hour > 12 or time_struct->tm_hour < 0) && ds3231_handle->hour_format == DS3231_TIME_FORMAT_12_HOURS)
+    {
+        ESP_LOGE(TAG, "Invalid hour value expected range (12h format): 0<=t<=12, got value: %i instead", time_struct->tm_hour);
+        return -1;
+    }
+
+    else if (time_struct->tm_wday > 7 or time_struct->tm_wday < 1)
+    {
+        ESP_LOGE(TAG, "Invalid weekday value expected range: 1<=t<=7, got value: %i instead", time_struct->tm_wday);
+        return -1;
+    }
+
+    else if (time_struct->tm_mday > 31 or time_struct->tm_mday < 1)
+    {
+        ESP_LOGE(TAG, "Invalid day of month value expected range: 1<=t<=31, got value: %i instead", time_struct->tm_mday);   
+        return -1;
+    }
+
+    else if (time_struct->tm_year-2000 > 99 or time_struct->tm_year-2000 < 0)
+    {
+        ESP_LOGE(TAG, "Invalid year value expected range: 0<=t<=99, got value: %i instead", time_struct->tm_year);
+        return -1;
+    }
+    else
+        return 0;
+}
+
+
+int ds3231_get_datetime(ds3231_handle_t *ds3231_handle, tm *time_struct)
+{
+    /*Get the date and time information from the ds3231*/
+
+    tm reference_time; // Creating internal tm struct to validate the ds3231 internal clock before rewriting the passed struct
+
+    int* time_addresses[] = 
+    {
+        &reference_time.tm_sec,
+        &reference_time.tm_min,
+        &reference_time.tm_hour,
+        &reference_time.tm_wday,
+        &reference_time.tm_mday,
+        &reference_time.tm_mon,
+        &reference_time.tm_year,
+    }; // Put all relevant struct pointers into an array for sequential iteration
+
+    uint8_t buffer[1]; // Buffer to store returned time byte
+    uint8_t register_address[1]; // Data buffer to be sent
+
+    int time_addresses_len = sizeof(time_addresses)/sizeof(time_addresses[0]); // Calculate the size of the array
+    char* TAG = "ds3231";
+    
+    for (uint8_t i = 0; i < time_addresses_len; i++)
+    {
+        register_address[0] = i; // The ds3231 time registers are ordered by ascending values (starting from 0 for seconds)
+        i2c_master_transmit_receive(ds3231_handle->dev_handle, register_address, 1, buffer, 1, -1); // Get relevant register info
+        *time_addresses[i] = (int)bcd_to_decimal(*buffer); // The data is stored in a bcd so it needs to be converted to decimal 
+    }
+    reference_time.tm_year += 2000; // The ds3231 stores the year data in a range of 0 to 99
+    
+    reference_time.tm_hour = (reference_time.tm_hour << 3) >> 3;
+
+
+    if (validate_time(ds3231_handle, &reference_time) != 0)
+    {
+        ESP_LOGE(TAG, "Recieved invalid time from module; Discarding changes...");
+        return -1;
+    }// TODO: ADD APPROPRIATE RETURN
+
+    *time_struct = reference_time; // Override the original time struct
+    return 0;
+}
+
+int ds3231_set_datetime(ds3231_handle_t *ds3231_handle, tm *time_struct)
+{
+    /*Set the date and time of the rtc module*/
+    char TAG[] = "ds3231";
+    if (validate_time(ds3231_handle, time_struct) == 0)
+    {
+        i2c_master_dev_handle_t dev_handle = ds3231_handle->dev_handle;
+
+        time_struct->tm_year -= 2000;
+        
+        int* time_addresses[] = 
+        {
+            &time_struct->tm_sec,
+            &time_struct->tm_min,
+            &time_struct->tm_hour,
+            &time_struct->tm_wday,
+            &time_struct->tm_mday,
+            &time_struct->tm_mon,
+            &time_struct->tm_year,
+        }; // Put all relevant struct pointers into an array for sequential iteration
+
+        int time_addresses_len = sizeof(time_addresses)/sizeof(time_addresses[0]); // Calculate the size of the array
+
+        for (uint8_t i = 0; i < time_addresses_len; i++)
+        {
+            uint8_t buffer[2] = {i, decimal_to_bcd(*time_addresses[i])}; // Convert time to bcd format
+            i2c_master_transmit(dev_handle, buffer, 2, -1); // Set registers
+        }
+    time_struct->tm_year += 2000;
+    }
+    else
+    ESP_LOGE(TAG, "Invalid time struct passed; Time was not set...");
+    return -1;
+}
+
+int str_to_month(const char* month)
+{
+    /*Convert a month string into a corresponding number*/
+    const char* months_of_year[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    int num_monthes = sizeof(months_of_year)/sizeof(months_of_year[0]);
+    for (int i = 0; i < num_monthes;i++)
+    {
+        if (strcmp(month, months_of_year[i]) == 0)
+        {
+            return i+1;
+        }
+        
+    }
+    return -1; 
+}
+
+void ds3231_set_datetime_at_compile(ds3231_handle_t *ds3231_handle)
+{
+    tm time_struct;
+    char date_buff[] = __DATE__; // Date during time (string)
+    char month[16]; // Temporary buffer to store month string
+    sscanf(date_buff, "%s %d %d", month, &time_struct.tm_mday, &time_struct.tm_year); // Extract month to buffer and day and year as intagers
+    time_struct.tm_mon = str_to_month(month); // Convert month string to numerical value
+    
+    if(validate_time(ds3231_handle, &time_struct) == 0) 
+    {
+        ds3231_set_datetime(ds3231_handle, &time_struct); // Set time on rtc if the time is valid
+    }
+}
---
+++ main/main.cpp
@@ -0,0 +1,355 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_intr_alloc.h"
+
+#include "driver/gpio.h"
+#include "driver/gptimer.h"
+#include "driver/i2c_master.h"
+
+#include "hagl_hal.h"
+#include "hagl.h"
+#include "font9x18B-ISO8859-1.h"
+#include "font6x9.h"
+
+#include "ds3231.h"
+#include "string.h"
+
+#define LED_PIN             GPIO_NUM_2
+#define GPIO_VIBRATOR_PIN   GPIO_NUM_25
+
+#define GPIO_BUTTON_PIN     GPIO_NUM_34
+#define ENCODER_CLK_PIN     GPIO_NUM_39
+#define ENCODER_DT_PIN      GPIO_NUM_36
+
+#define I2C_SCL_PIN         GPIO_NUM_22
+#define I2C_SDA_PIN         GPIO_NUM_21
+#define I2C_FREQ_HZ         1000000
+
+#define OLED_REFRESH_RATE   60
+
+#define BUTTON_DEBOUNCE_COOLDOWN    10000
+#define BUTTON_LONG_PRESS_COOLDOWN  500000
+
+#define SET_COMPILE_TIME_FOR_RTC    false
+
+#define SCEEN_SIZE_X 240
+#define SCEEN_SIZE_Y 320
+
+hagl_backend_t display; // Main display 
+
+gptimer_handle_t button_timer; // Timer for button software debounce
+
+ds3231_handle_t ds3231_dev_handle; // Rtc
+
+tm rtc_time {0,0,0,0,0,0,0,0}; // Time from the rtc module
+
+typedef enum : uint8_t{
+    FOO,
+    SHORT_PRESS_EVENT,
+    LONG_PRESS_EVENT,
+    SCROLL_UP_EVENT,
+    SCROLL_DOWN_EVENT,
+}input_event_t; // Input event flags for the input events queue
+
+QueueHandle_t input_event_queue = xQueueCreate(10, sizeof(input_event_t)); // Queue for input events (used in input isrs and input event task)
+
+static void IRAM_ATTR gpio_button_isr_handler(void* arg)
+{
+    gptimer_stop(button_timer); // Stop timer before setting counting start
+
+    if (gpio_get_level(GPIO_BUTTON_PIN))
+    {
+        gptimer_set_raw_count(button_timer, BUTTON_DEBOUNCE_COOLDOWN); // Reset timer count
+    }
+    else
+    {
+        gptimer_set_raw_count(button_timer, BUTTON_LONG_PRESS_COOLDOWN); // Start long press cooldown if button is pressed
+    }
+
+    gptimer_start(button_timer); // Start timer
+}
+
+static bool IRAM_ATTR button_timer_isr_handler(gptimer_handle_t timer, const gptimer_alarm_event_data_t* edata, void* arg)
+{
+    static bool disable_short_press = false;
+    input_event_t event;
+    BaseType_t should_yield = pdFALSE;
+
+    if (gpio_get_level(GPIO_BUTTON_PIN)) // When button is depressed (Short press behavior)
+    {
+        if (!disable_short_press)
+        {
+            event = SHORT_PRESS_EVENT;
+            xQueueSendFromISR(input_event_queue, &event, &should_yield); // Pose event to queue
+        }
+        else // Re-enable short press behavior
+        {
+            disable_short_press = false;
+        }
+    }
+
+    else // If button is pressed (Long press behavior)
+    {
+        disable_short_press = true; // Disable short press once to prevent both press types occuring at once
+        event = LONG_PRESS_EVENT;
+        xQueueSendFromISR(input_event_queue, &event, &should_yield); // Pose event to queue
+    }
+
+    if (should_yield != pdFALSE)
+    {
+       portYIELD_FROM_ISR();
+    }
+
+    return false;
+}
+
+static void IRAM_ATTR gpio_encoder_isr_handler(void* arg)
+{
+/*
+This function reads the states of the encoder pins and compares their states
+Since the contacts connected to the pins are offset by 90 degrees inside the encoder,
+one of them will always change before the other when rotating clockwise and vise-versa
+*/
+    bool CLK_state = gpio_get_level(ENCODER_CLK_PIN);
+    static bool reference_CLK_state = false;
+    
+    input_event_t event;
+    BaseType_t should_yield = pdFALSE;
+
+    if (reference_CLK_state != CLK_state)
+    {
+        reference_CLK_state = CLK_state;
+
+        if (CLK_state != gpio_get_level(ENCODER_DT_PIN))
+        {
+            event = SCROLL_UP_EVENT;
+            xQueueSendFromISR(input_event_queue, &event, &should_yield);
+        }
+        else
+        {
+            event = SCROLL_DOWN_EVENT;
+            xQueueSendFromISR(input_event_queue, &event, &should_yield);
+        }        
+
+    }
+
+    if (should_yield != pdFALSE)
+    {
+        portYIELD_FROM_ISR();
+    }
+}
+
+void input_events_handler(void* arg)
+{
+    /*This tasks is responisble for handling the input events from buttons and encoders.
+    It gets the info from a queue to which the input ISR's handlers send a message when they are activated*/
+    
+    input_event_t event; // Event type
+    char* event_id = "";
+    while(1){
+        if(xQueueReceive(input_event_queue, &event, portMAX_DELAY)) // Wait for an event in the queue
+        {
+            switch (event)
+            {
+                case SHORT_PRESS_EVENT:
+                    static bool led_state = false;
+                    led_state = !led_state;
+                    gpio_set_level(LED_PIN, led_state);
+                    event_id = "SHORT_PRESS_EVENT";
+                    break;
+                
+                case LONG_PRESS_EVENT:
+                    static bool vibrator_state = false;
+                    vibrator_state = !vibrator_state;
+                    gpio_set_level(GPIO_VIBRATOR_PIN, vibrator_state);
+                    event_id = "LONG_PRESS_EVENT";
+                    break;
+
+                case SCROLL_UP_EVENT:
+                    event_id = "SCROLL_UP_EVENT";
+                    break;
+
+                case SCROLL_DOWN_EVENT:
+                    event_id = "SCROLL_DOWN_EVENT";
+                    break;
+                default:
+                    break;
+            }
+        ESP_LOGI("input_event", "%s", event_id);
+        ESP_LOGI("task_size", "%i", uxTaskGetStackHighWaterMark(NULL));
+        }
+    }
+}
+
+void write_time(void* arg)
+{
+    const TickType_t task_delay_ms = 1000 / portTICK_PERIOD_MS;
+
+    hagl_color_t color = hagl_color(&display, 255, 0, 0); // Placeholder
+    char time_str [32]; // String buffer
+    wchar_t formatted_str [32]; // String buffer compatable with the display library
+    while (1)
+    {
+        ds3231_get_datetime(&ds3231_dev_handle, &rtc_time); // Get time
+        snprintf(time_str, 64, "%02i:%02i:%02i", 
+        rtc_time.tm_hour, rtc_time.tm_min, rtc_time.tm_sec);
+        mbstowcs(formatted_str, time_str, 32);
+        hagl_put_text(&display, formatted_str, DISPLAY_WIDTH/2 - strlen(time_str)*4, (DISPLAY_HEIGHT/2 - strlen(time_str)*9), color, font9x18B_ISO8859_1); // Display string
+        
+        snprintf(time_str, 64, "%02i,%02i,%04i", 
+        rtc_time.tm_mday, rtc_time.tm_mon, rtc_time.tm_year);
+        mbstowcs(formatted_str, time_str, 32);
+        hagl_put_text(&display, formatted_str, DISPLAY_WIDTH/2 - strlen(time_str)*4, (DISPLAY_HEIGHT/2 - strlen(time_str)*9) + 40, color, font9x18B_ISO8859_1); // Display string
+
+        vTaskDelay(task_delay_ms); // Delay for 1 second
+    }
+    ESP_LOGI("task_size", "%i", uxTaskGetStackHighWaterMark(NULL));
+}
+gptimer_handle_t setup_gptimer()
+{
+    /*Timer interrupt setup (for button)*/
+
+    ESP_LOGI("Setup", "Setting up timer");
+
+    gptimer_handle_t gptimer;
+    gptimer_config_t button_timer_conf = 
+    {
+        .clk_src = GPTIMER_CLK_SRC_DEFAULT,
+        .direction = GPTIMER_COUNT_DOWN,
+        .resolution_hz = 1000000,
+        .intr_priority = 0,
+    };
+
+    ESP_ERROR_CHECK(gptimer_new_timer(&button_timer_conf, &gptimer)); // Create timer object
+
+    gptimer_alarm_config_t button_timer_alarm_conf = 
+    {
+        .alarm_count = 0,
+        .reload_count = 0,
+    };
+    button_timer_alarm_conf.flags.auto_reload_on_alarm = false;
+
+    ESP_ERROR_CHECK(gptimer_set_alarm_action(gptimer, &button_timer_alarm_conf));
+    gptimer_event_callbacks_t cbs = 
+    {
+        .on_alarm = button_timer_isr_handler,
+    };
+
+    ESP_ERROR_CHECK(gptimer_register_event_callbacks(gptimer, &cbs, NULL));
+
+    ESP_ERROR_CHECK(gptimer_enable(gptimer));
+    return gptimer;
+}
+
+void setup_gpio()
+{
+    /*Configure the gpio of the esp*/
+    ESP_LOGI("Setup", "Setting up GPIO");
+
+    // LED setup
+    gpio_config_t led_io_conf =
+    {
+        .pin_bit_mask = (1ULL << LED_PIN | 1ULL << GPIO_VIBRATOR_PIN), // Bitmask to select the GPIO pins
+        .mode = GPIO_MODE_OUTPUT, // Set as output mode
+        .pull_up_en = GPIO_PULLUP_DISABLE, // Disable pull-up resistor
+        .pull_down_en = GPIO_PULLDOWN_DISABLE, // Disable pull-down resistor
+        .intr_type = GPIO_INTR_DISABLE, // Disable interrupt
+    };
+    gpio_config(&led_io_conf); // Apply configuration
+    
+    // Button setup
+    gpio_config_t button_io_conf =
+    {
+        .pin_bit_mask = (1ULL << GPIO_BUTTON_PIN), 
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE, 
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&button_io_conf); 
+
+    // Encoder pins setup
+    gpio_config_t encoder_io_conf = 
+    {
+        .pin_bit_mask = (1ULL << ENCODER_CLK_PIN | 1ULL << ENCODER_DT_PIN),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&encoder_io_conf);
+}
+
+void setup_isr()
+{
+    ESP_LOGI("Setup", "Setting up interrupts");
+
+    /*Set up the isrs of the input devices*/
+    ESP_ERROR_CHECK(gpio_install_isr_service(ESP_INTR_FLAG_IRAM)); // Set up interrupt
+
+    // Button isr
+    ESP_ERROR_CHECK(gpio_set_intr_type(GPIO_BUTTON_PIN, GPIO_INTR_ANYEDGE)); // Interrupt will be called on button release since 
+    ESP_ERROR_CHECK(gpio_isr_handler_add(GPIO_BUTTON_PIN, gpio_button_isr_handler, NULL)); // Link to interrupt handler & pass pointer to the timer object when called
+
+    // Encoder (DT pin) isr
+    ESP_ERROR_CHECK(gpio_set_intr_type(ENCODER_DT_PIN, GPIO_INTR_ANYEDGE));
+    ESP_ERROR_CHECK(gpio_isr_handler_add(ENCODER_DT_PIN, gpio_encoder_isr_handler, NULL));
+
+    // Encoder (CLK pin) isr
+    ESP_ERROR_CHECK(gpio_set_intr_type(ENCODER_CLK_PIN, GPIO_INTR_ANYEDGE));
+    ESP_ERROR_CHECK(gpio_isr_handler_add(ENCODER_CLK_PIN, gpio_encoder_isr_handler, NULL));    
+}
+
+i2c_master_bus_handle_t setup_i2c_master()
+{
+    // Set up the i2c master bus
+    i2c_master_bus_config_t i2c_master_conf =
+    {
+        .i2c_port = 1,
+        .sda_io_num = I2C_SDA_PIN,
+        .scl_io_num = I2C_SCL_PIN,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+        .intr_priority = 0,
+    };
+    i2c_master_conf.flags.enable_internal_pullup = true;
+    i2c_master_bus_handle_t i2c_master_handle;
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_master_conf, &i2c_master_handle));
+    return i2c_master_handle;
+}
+
+extern "C" void app_main(void)
+{
+    setup_gpio(); // Set up the gpio pins for inputs
+    button_timer = setup_gptimer(); // Create timer for software debounce
+    setup_isr(); // Set up input interrupts
+
+    // Task to handle the various input interrupt signals
+    TaskHandle_t input_events_handler_task;
+    xTaskCreatePinnedToCore(input_events_handler, "write_time_task", 4096, NULL, 4, &input_events_handler_task, 0);
+
+    // i2c setup
+    i2c_master_bus_handle_t i2c_master_handle = setup_i2c_master();
+    
+    // Rtc setup
+    ds3231_dev_handle = ds3231_init(&i2c_master_handle, DS3231_TIME_FORMAT_24_HOURS); // rtc setup
+    ds3231_set_datetime(&ds3231_dev_handle, &rtc_time);
+
+    // Set Rtc time to compilation time
+    if (SET_COMPILE_TIME_FOR_RTC)
+    {
+        ds3231_set_datetime_at_compile(&ds3231_dev_handle);
+    }
+
+    // Display setup
+    display = *hagl_init();
+    hagl_clear(&display);
+    
+    // Time-keeping task
+    TaskHandle_t write_time_task;
+    xTaskCreatePinnedToCore(write_time, "write_time_task", 4096, NULL, 4, &write_time_task, 0);
+    }
